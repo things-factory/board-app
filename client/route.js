8970d7592ef00ac3456ae27a8db2a0a1dfc72eec
Hearty Oh <heartyoh@hatiolab.com>
2019-07-25 11:48:41 +0900

    Initial commit

---
+++ client/route.js
@@ -0,0 +1,15 @@
+export default function route(page) {
+  switch (page) {
+    case 'index':
+      /* board-list 페이지를 default page로 한다. */
+      return 'board-list'
+
+    case 'board-list':
+      import('./pages/board-list-page')
+      return page
+
+    case 'play-list':
+      import('./pages/play-list-page')
+      return page
+  }
+}
---
+++ things-factory.config.js
@@ -0,0 +1,17 @@
+import route from './client/route'
+import bootstrap from './client/bootstrap'
+
+export default {
+  route,
+  routes: [
+    {
+      tagname: 'board-list-page',
+      page: 'board-list'
+    },
+    {
+      tagname: 'play-list-page',
+      page: 'play-list'
+    }
+  ],
+  bootstrap
+}
