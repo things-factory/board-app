280ffc0e89fd18fa75f6d969f681322a203111a0
Hearty Oh <heartyoh@hatiolab.com>
2019-10-08 12:29:03 +0800

    v0.4.0

--- client/pages/font-list-page.js
+++ client/pages/font-list-page.js
@@ -1,7 +1,6 @@
 import { html, css } from 'lit-element'
 import { PageView } from '@things-factory/shell'
-// import '@things-factory/font-ui'
-import '../font-list/font-selector'
+import '@things-factory/font-ui'
 
 export class FontListPage extends PageView {
   static get styles() {
@@ -33,12 +32,6 @@ export class FontListPage extends PageView {
     return html`
       <font-selector .creatable=${true}></font-selector>
     `
-  }
-
-  async pageInitialized() {
-    await this.updateComplete
-
-    this.shadowRoot.querySelector('font-selector').refreshFonts()
   }
 }
 
