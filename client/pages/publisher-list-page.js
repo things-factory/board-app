37f73d3d4568671cdb2ead939bc3143be1de196b
Hearty Oh <heartyoh@hatiolab.com>
2019-10-10 02:25:38 +0800

    publisher

--- client/pages/publisher-list-page.js
+++ client/pages/publisher-list-page.js
@@ -1,18 +1,18 @@
-import '@material/mwc-button'
-import '@material/mwc-fab'
+import { css, html } from 'lit-element'
 import '@material/mwc-icon'
-import { appendViewpart, toggleOverlay, VIEWPART_POSITION } from '@things-factory/layout-base'
+
+import { PageView, ScrollbarStyles, isMobileDevice } from '@things-factory/shell'
+import { i18next } from '@things-factory/i18n-base'
+import '@things-factory/grist-ui'
+import '@things-factory/form-ui'
 import {
+  createPublisher,
   deletePublishers,
   fetchPublisherList,
   updatePublisher
 } from '@things-factory/publisher-base/client/graphql/publisher'
-import '@things-factory/publisher-ui'
-import { InfiniteScrollable, PageView, pulltorefresh, ScrollbarStyles, store } from '@things-factory/shell'
-import { css, html } from 'lit-element'
-import { connect } from 'pwa-helpers/connect-mixin.js'
 
-class PublisherListPage extends connect(store)(InfiniteScrollable(PageView)) {
+class PublisherListPage extends PageView {
   static get styles() {
     return [
       ScrollbarStyles,
@@ -20,222 +20,207 @@ class PublisherListPage extends connect(store)(InfiniteScrollable(PageView)) {
         :host {
           display: flex;
           flex-direction: column;
-          position: relative;
 
           overflow: hidden;
         }
 
-        #create {
-          position: absolute;
-          bottom: 15px;
-          right: 16px;
+        search-form {
+          overflow: visible;
+        }
+
+        data-grist {
+          overflow-y: hidden;
+          flex: 1;
         }
       `
     ]
   }
 
   static get properties() {
-    return {
-      publishers: Array,
-      _page: Number,
-      _total: Number
-    }
+    return {}
   }
 
   get context() {
     return {
       title: 'Publisher List',
-      board_topmenu: true
-    }
-  }
-
-  constructor() {
-    super()
-
-    appendViewpart({
-      name: 'create-publisher',
-      viewpart: {
-        show: false,
-        hovering: 'center',
-        template: html`
-          <create-publisher
-            @create-success=${e => {
-              this.onCreateSucceeded(e)
-            }}
-          ></create-publisher>
-        `
+      board_topmenu: true,
+      actions: [
+        {
+          title: i18next.t('button.save'),
+          action: this.savePublishers.bind(this)
+        },
+        {
+          title: i18next.t('button.delete'),
+          action: this.deletePublishers.bind(this)
+        }
+      ],
+      exportable: {
+        name: i18next.t('title.publisher'),
+        data: this.exportables.bind(this)
       },
-      position: VIEWPART_POSITION.ASIDEBAR
-    })
-
-    this._page = 1
-    this._total = 0
-
-    this.publishers = []
-
-    this._infiniteScrollOptions.limit = 50
-  }
-
-  get scrollTargetEl() {
-    return this.renderRoot.querySelector('#list')
+      importable: {
+        handler: this.importables.bind(this)
+      }
+    }
   }
 
   render() {
     return html`
-      <publisher-list
-        .publishers=${this.publishers}
-        @start-publisher=${e => this.onStartPublisher(e)}
-        @stop-publisher=${e => this.onStopPublisher(e)}
-        @publisher-delete=${e => {
-          var detail = e.detail
-          var ids = detail.ids
-          this.onDeletePublishers(ids)
-        }}
-        @publisher-edited=${e => this.onEditPublisher(e)}
-      ></publisher-list>
+      <search-form .fields=${this._searchFields} @submit=${e => this.grist.fetch()}></search-form>
 
-      <mwc-fab id="create" icon="add" title="create" @click=${e => this.showCreatePublisherView()}> </mwc-fab>
+      <data-grist
+        .mode=${isMobileDevice() ? 'LIST' : 'GRID'}
+        .config=${this._gristConfig}
+        .fetchHandler=${this.fetchHandler.bind(this)}
+      ></data-grist>
     `
   }
 
-  async refresh() {
-    await this.refreshPublishers()
-  }
-
-  async getPublishers({ page = 1, limit = this._infiniteScrollOptions.limit } = {}) {
-    var listParam = {
-      filters: [
-        // {
-        //   name: 'group_id',
-        //   operator: 'eq',
-        //   value: this.groupId
-        // }
-      ],
-      sortings: [
-        {
-          name: 'name',
-          desc: true
-        }
-      ],
-      pagination: {
-        page,
-        limit
-      }
-    }
-
-    var result = await fetchPublisherList(listParam)
-    return result.publishers || []
-  }
-
-  async refreshPublishers() {
-    var { items: publishers, total } = await this.getPublishers()
-
-    this.publishers = [...publishers]
-    this._page = 1
-    this._total = total
-  }
-
-  async appendPublishers() {
-    var { items: publishers, total } = await this.getPublishers({
-      page: this._page + 1
+  async fetchHandler({ page, limit, sorters = [] }) {
+    const result = await fetchPublisherList({
+      filters: this.searchForm.queryFilters || [],
+      pagination: { page, limit },
+      sortings: sorters
     })
 
-    this.publishers = [...this.publishers, ...publishers]
-    this._page = this._page + 1
-    this._total = total
+    var { total, items: records } = result && result.publishers
+
+    return { total, records }
   }
-
-  async scrollAction() {
-    return this.appendPublishers()
-  }
-
-  updated(change) {}
-
-  stateChanged(state) {}
 
   async pageInitialized() {
+    this._searchFields = this.searchFields
+    this._gristConfig = this.gristConfig
+
     await this.updateComplete
 
-    this.refreshPublishers()
+    this.grist.fetch()
   }
 
-  firstUpdated() {
-    pulltorefresh({
-      container: this.shadowRoot,
-      scrollable: this.scrollTargetEl,
-      refresh: () => {
-        return this.refresh()
+  get grist() {
+    return this.shadowRoot.querySelector('data-grist')
+  }
+
+  get searchForm() {
+    return this.shadowRoot.querySelector('search-form')
+  }
+
+  get searchFields() {
+    return [
+      {
+        label: i18next.t('field.name'),
+        name: 'name',
+        type: 'text',
+        props: { searchOper: 'like' }
+      },
+      {
+        label: i18next.t('field.description'),
+        name: 'description',
+        type: 'text',
+        props: { searchOper: 'like' }
+      },
+      {
+        label: i18next.t('field.status'),
+        name: 'status',
+        type: 'text',
+        props: { searchOper: 'like' }
       }
-    })
+    ]
   }
 
-  async onDeletePublishers(publisherIds) {
-    try {
-      await deletePublishers(publisherIds)
+  get gristConfig() {
+    return {
+      rows: { selectable: { multiple: true }, appendable: true },
+      columns: [
+        { type: 'gutter', gutterName: 'dirty' },
+        { type: 'gutter', gutterName: 'row-selector', multiple: true },
+        {
+          type: 'gutter',
+          gutterName: 'button',
+          icon: record => (!record ? 'play_arrow' : record.status == 1 ? 'pause' : 'play_arrow'),
+          handlers: {
+            click: (columns, data, column, record, rowIndex) => {
+              if ((record.status = 0)) {
+                this.startPublisher(record)
+              } else {
+                this.stopPublisher(record)
+              }
+            }
+          }
+        },
+        {
+          type: 'string',
+          name: 'name',
+          header: i18next.t('field.name'),
+          record: { editable: true, align: 'center' },
+          sortable: true,
+          width: 200
+        },
+        {
+          type: 'string',
+          name: 'description',
+          header: i18next.t('field.description'),
+          record: { editable: true, align: 'left' },
+          sortable: true,
+          width: 150
+        },
+        {
+          type: 'string',
+          name: 'intervalExpr',
+          header: i18next.t('field.crontab'),
+          record: { editable: true, align: 'left' },
+          width: 100
+        },
+        {
+          type: 'string',
+          name: 'apiUrl',
+          header: i18next.t('field.api_url'),
+          record: { editable: true, align: 'left' },
+          width: 150
+        },
+        {
+          type: 'string',
+          name: 'status',
+          header: i18next.t('field.status'),
+          record: { align: 'center' },
+          width: 40
+        },
+        {
+          type: 'datetime',
+          name: 'updatedAt',
+          header: i18next.t('field.updated_at'),
+          record: { editable: false, align: 'center' },
+          sortable: true,
+          width: 150
+        },
+        {
+          type: 'object',
+          name: 'updater',
+          header: i18next.t('field.updater'),
+          record: { editable: false, align: 'center' },
+          sortable: true,
+          width: 150
+        }
+      ]
+    }
+  }
 
-      document.dispatchEvent(
-        new CustomEvent('notify', {
-          detail: {
-            level: 'info',
-            message: 'deleted'
-          }
-        })
-      )
-    } catch (ex) {
-      document.dispatchEvent(
-        new CustomEvent('notify', {
-          detail: {
-            level: 'error',
-            message: ex,
-            ex: ex
-          }
-        })
-      )
+  async savePublishers() {
+    let patches = this.grist.exportPatchList({ flagName: 'cuFlag' })
+    if (!patches || patches.length == 0) {
+      return
     }
 
-    this.refreshPublishers()
-  }
-
-  onStartPublisher(e) {
-    var { publisher } = e.detail
-
-    this.startPublisher(publisher)
-  }
-
-  onStopPublisher(e) {
-    var { publisher } = e.detail
-
-    this.stopPublisher(publisher)
-  }
-
-  async startPublisher(publisher) {
-    await fetch(`/start-publisher/${publisher.id}`)
-    this.refreshPublishers()
-  }
-
-  async stopPublisher(publisher) {
-    await fetch(`/stop-publisher/${publisher.id}`)
-    this.refreshPublishers()
-  }
-
-  showCreatePublisherView() {
-    toggleOverlay('create-publisher')
-  }
-
-  onCreateSucceeded(e) {
-    var detail = e.detail
-    var publisher = detail.publisher
-
-    toggleOverlay('create-publisher')
-
-    this.refreshPublishers()
-  }
-
-  async onEditPublisher(e) {
-    var { publisher, data } = e.detail
-
     try {
-      await updatePublisher(Object.assign({}, publisher, data))
+      await Promise.all(
+        patches.map(async patch => {
+          if (patch.cuFlag == 'M') {
+            await updatePublisher(patch)
+          } else {
+            await createPublisher(patch)
+          }
+        })
+      )
 
       document.dispatchEvent(
         new CustomEvent('notify', {
@@ -257,7 +242,94 @@ class PublisherListPage extends connect(store)(InfiniteScrollable(PageView)) {
       )
     }
 
-    this.refreshPublishers()
+    this.grist.fetch()
+  }
+
+  async deletePublishers() {
+    try {
+      const ids = this.grist.selected.map(record => record.id)
+      if (ids && ids.length > 0) {
+        await deletePublishers(ids)
+
+        document.dispatchEvent(
+          new CustomEvent('notify', {
+            detail: {
+              level: 'info',
+              message: 'deleted'
+            }
+          })
+        )
+
+        this.grist.fetch()
+      }
+    } catch (ex) {
+      document.dispatchEvent(
+        new CustomEvent('notify', {
+          detail: {
+            level: 'error',
+            message: ex,
+            ex: ex
+          }
+        })
+      )
+    }
+  }
+
+  async startPublisher(publisher) {
+    await fetch(`/start-publisher/${publisher.id}`)
+    this.grist.fetch()
+  }
+
+  async stopPublisher(publisher) {
+    await fetch(`/stop-publisher/${publisher.id}`)
+    this.grist.fetch()
+  }
+
+  async importHandler(patches) {
+    const response = await client.query({
+      query: gql`
+          mutation {
+            updateMultiplePublisher(${gqlBuilder.buildArgs({
+              patches
+            })}) {
+              name
+            }
+          }
+        `
+    })
+
+    if (!response.errors) {
+      history.back()
+      this.dataGrist.fetch()
+      document.dispatchEvent(
+        new CustomEvent('notify', {
+          detail: {
+            message: i18next.t('text.data_imported_successfully')
+          }
+        })
+      )
+    }
+  }
+
+  importables(records) {
+    openPopup(
+      html`
+        <import-pop-up
+          .records=${records}
+          .config=${this.config}
+          .importHandler=${this.importHandler.bind(this)}
+        ></import-pop-up>
+      `,
+      {
+        backdrop: true,
+        size: 'large',
+        title: i18next.t('title.import')
+      }
+    )
+  }
+
+  exportables() {
+    return this.grist.exportRecords()
   }
 }
 
