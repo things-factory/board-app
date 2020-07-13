c2c1b38bbd8c6659f4341a66c1da2893f751cec6
Hearty Oh <heartyoh@hatiolab.com>
2020-07-13 15:42:27 +0900

    auto start active connection and scenario

--- server/index.ts
+++ server/index.ts
@@ -1,2 +1,15 @@
+import { Connections, ScenarioEngine } from '@things-factory/integration-base'
+
 export * from './migrations'
 export * from './graphql'
+
+process.on('bootstrap-module-start' as any, async ({ app, config, client }: any) => {
+  console.log('%%%%%%%%%%%%%%%% SCENARIO ENGINE - BEGIN %%%%%%%%%%%%%%%%')
+  try {
+    await Connections.ready()
+    await ScenarioEngine.loadAll()
+  } catch (ex) {
+    Connections.logger.error(ex)
+  }
+  console.log('%%%%%%%%%%%%%%%% SCENARIO ENGINE - END %%%%%%%%%%%%%%%%')
+})
